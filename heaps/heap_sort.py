cb3b3874ac292081af63f3996c9b13f938959821
Chris McAnally <chris@adadevelopersacademy.org>
2021-08-05 10:51:28 -0700

    initial heapsort starter code

--- heaps/heap_sort.py
+++ heaps/heap_sort.py
@@ -1,8 +1,8 @@
 
 
-# This method uses a heap to sort an array.
-# Time Complexity:  ?
-# Space Complexity: ?
-def heap_sort(list)
-  raise NotImplementedError, "Method not implemented yet..."
-end
+def heap_sort(list):
+    """ This method uses a heap to sort an array.
+        Time Complexity:  ?
+        Space Complexity: ?
+    """
+    pass
